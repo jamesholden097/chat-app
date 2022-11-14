3f85facb0a04b319d89618ed2377f1e7d0fdf48d
Muhammad Samin Hasan <53146728+saminhasan@users.noreply.github.com>
2022-11-14 15:38:23 +0600

    bug fixes

--- client.py
+++ client.py
@@ -1,9 +1,9 @@
 import sys
 import time
 import pickle
-import signal
 import socket
-import threading
+import traceback
+from threading import Thread
 from util import HEADERSIZE, data_packet, all_keys
 
 class Client:
@@ -13,20 +13,51 @@ class Client:
         self.port = port
         self.address = (self.ip, self.port)
         self.connect_server()
+        self.connected = False
         self.running = True
 
     def connect_server(self):
-        self.socket_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.socket_client.connect(self.address)
+        try:
+            self.socket_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.socket_client.connect(self.address)
+            self.connected = True
+
+        except ConnectionRefusedError:
+            print("Server Down")
+            
+    def start(self):
+        if self.connected:
+            self.client_thread = Thread(target=self.recieve_data, args=[self.socket_client])
+            self.client_thread.start()
+
+    def run(self):
+        try:
+            while self.running:
+                inp = input("Press enter send data : ")
+                self.send_data(self.socket_client, inp)
+                if inp == "<DIS>":
+                    self.close()
+        except KeyboardInterrupt:
+            self.running = False
 
 
     def recieve_data(self, socket_connection):
-        while self.running:
-            full_message = b''
-            new_message = True
-            message_length = 0
-            while True:
-                message = socket_connection.recv(HEADERSIZE + 4)
+
+        full_message = b''
+        new_message = True
+        message_length = 0
+        while self.running and self.connected:
+            try:
+                message = socket_connection.recv(HEADERSIZE * 2)
+            except ConnectionAbortedError:
+                print("Closing client.")
+                self.close()
+
+            except Exception as error:
+                self.running = False
+                traceback.print_exc()
+
+            else:
                 if new_message:
                     if message[:HEADERSIZE]:
                         message_length = int(message[:HEADERSIZE])
@@ -34,45 +65,38 @@ class Client:
 
                 full_message += message
                 if len(full_message) - HEADERSIZE == message_length:
-                    print(pickle.loads(full_message[HEADERSIZE:]))
+                    # print(pickle.loads(full_message[HEADERSIZE:]))
+                    self.process_message(full_message)
                     new_message = True
                     full_message = b""
+                    message_length = 0
 
+        self.close()
 
-
-    def run(self):
-        try:
-            while self.running:
-                s_thread = threading.Thread(target=self.recieve_data, args=[self.socket_client])
-                s_thread.start()
-                d= data_packet
-                for key in list(all_keys(d)):
-                    d[key] = input(f"Enter value of  {key}: ")
-                #time.sleep(1)
-                #inp = input("Press enter send data : ")
-                #d = data_packet
-                self.send_data(self.socket_client,d)
-        except Exception as error:
-            print(f"ERROR : {error}")
-        finally:
-            self.close()
             
             
     def send_data(self, socket_connection, data):
         message = pickle.dumps(data)
         message = bytes(f"{len(message):<{HEADERSIZE}}", 'utf-8') + message
-        socket_connection.send(message)
-        print(f"Sent Data from client to  server {socket_connection.getpeername()}: {data}")
-        
+        try:
+            socket_connection.send(message)
+            # print(f"Sent Data from client to  server {socket_connection.getpeername()}: {data}")
+        except Exception as error:
+            print(error)
+            print("Error while sending")
+
+    def process_message(self, recieved_data):
+        print(f" Recieved_data {recieved_data} -> size : {round(sys.getsizeof(recieved_data)*1e-3, 2)} KB") # start processing command here
+
     def close(self):
         self.running = False
+        self.connected = False
+        self.socket_client.close()
 
-def signal_handler(signal, frame):
-    sys.exit(0)
+
     
 if __name__ == '__main__':
-
-    signal.signal(signal.SIGINT, lambda signal, frame : signal_handler(signal, frame))
     print("Client Launched")
     client = Client()
+    client.start()
     client.run()
--- server.py
+++ server.py
@@ -3,41 +3,62 @@ import pickle
 import signal
 import socket
 import threading
+import traceback
+
 from util import HEADERSIZE, data_packet
 
 class Server:
-    def __init__(self, ip=None, port=9999) -> None:
-        if not ip:
-            self.ip = socket.gethostbyname(socket.gethostname())
+    def __init__(self, ip='0.0.0.0', port=9999) -> None:
+        #if not ip:
+        self.ip = ip #socket.gethostbyname(socket.gethostname())
         self.port = port
 
         self.address = (self.ip, self.port)
-        self.socket_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.socket_server.bind(self.address)
+        self.socket_server = socket.socket(socket.AF_INET, socket.SOCK_STREAM) # try except else finally
+        self.socket_server.bind(self.address) # try except else finally
         self.clients = set()
         self.running = True
+
+    def start(self):
+        self.server_thread = threading.Thread(target=self.run, args=[])
+        self.server_thread.start()
 
     def run(self):
         self.socket_server.listen()
         while self.running:
-            socket_connection, address = self.socket_server.accept() 
+            socket_connection, address = self.socket_server.accept()  # try except else finally
             threading.Thread(target=self.handle_client, args=[socket_connection, address]).start()
             self.clients.add(socket_connection)
             print(f"Active Connections {threading.activeCount() - 1}")
-            print("New client connected.")
-            self.send_data(socket_connection, "Hello, I am server.")
+            print(len(self.clients))
+            print(self.clients)
+            # self.send_data(socket_connection, "Hello, I am server.")
 
 
 
     def handle_client(self, socket_connection, address):
+        # print(f"client socket_connection : {socket_connection}")
         print(f"client address : {address}")
+
         connected = True
-        while connected:
-            full_message = b''
-            new_message = True
-            message_length = 0
-            while self.running:
-                message = socket_connection.recv(HEADERSIZE + 4)
+        full_message = b''
+        new_message = True
+        message_length = 0
+        while self.running and connected:
+            try:
+                message = socket_connection.recv(HEADERSIZE * 2)
+
+            except ConnectionResetError:
+                print(f"client diconnected")
+                connected = False
+                break # just for now
+
+            #except Exception as error:
+            #    print("Exception")
+            #    traceback.print_exc()
+            #    break # just for now
+
+            else:
                 if new_message:
                     if message[:HEADERSIZE]:
                         message_length = int(message[:HEADERSIZE])
@@ -46,24 +67,28 @@ class Server:
                 if len(full_message) - HEADERSIZE == message_length:
                     recieved_data = pickle.loads(full_message[HEADERSIZE:])
                     self.process_message(address, recieved_data)
-
-                    if recieved_data == "<DISCONNECT>":
-                        connected = False
-                        socket_connection.close()
                     new_message = True
                     full_message = b""
+                if recieved_data == "<DIS>":
+                    print(f"Disconnect Request from client {socket_connection}")
+                    connected = False
 
+        self.clients.remove(socket_connection)
+        socket_connection.close()
 
 
     def send_data(self, client_socket, data):
         message = pickle.dumps(data)
         message = bytes(f"{len(message):<{HEADERSIZE}}", 'utf-8') + message
-        client_socket.send(message)
-        
-        print(f"Sent Data from server to {client_socket.getpeername()}: {data}")
+        try:
+            client_socket.send(message)
+            # print(f"Sent Data from client to  server {socket_connection.getpeername()}: {data}")
+        except Exception as error:
+            print(error)
+            print("Error while sending")
     
     def process_message(self, address, recieved_data):
-        print(f"from : {address} -> data : {recieved_data} -> size : {round(sys.getsizeof(recieved_data)*1e-6, 2)} MB")
+        print(f"from : {address} -> data : {recieved_data} -> size : {round(sys.getsizeof(recieved_data)*1e-3, 2)} KB") # start processing commands here
         
     def close(self):
         self.running = False
@@ -75,4 +100,5 @@ if __name__ == '__main__':
     signal.signal(signal.SIGINT, lambda signal, frame : signal_handler(signal, frame))
     print("Server Launched")
     server = Server()
-    server.run()
+    server.start()
+    server.server_thread.join()
